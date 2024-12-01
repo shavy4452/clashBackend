41d9f51caa24c35924ac4b554811acf8cc11f6f3
Shavy <shavygaming@gmail.com>
2024-12-01 07:05:42 +0530

    Removed Exposes

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ APP_NAME="clashBackend"       # Application name
 REPO_URL="git@github.com:shavy4452/clashBackend.git" # Replace with your repository URL
 APP_DIR="/data/$APP_NAME"     # Application directory
 BRANCH="main"                    # Branch to deploy
+CONFIG_DIR="/data/config"      # Configuration directory
+CONFIG_FILE="config.js"      # Configuration file
 
 echo "Starting deployment of $APP_NAME..."
 
@@ -35,6 +37,43 @@ if [ -f "package.json" ] && grep -q "\"build\"" package.json; then
     echo "Building application..."
     npm run build
 fi
+
+create_config() {
+    create_config() {
+    echo "Creating $CONFIG_FILE..."
+    mkdir -p $CONFIG_DIR
+    cat <<EOL > $CONFIG_FILE
+const config = {
+    isProduction: true,
+    env: "prod",
+    port: 2229,
+    domain: "http://localhost:2229",
+    db: {
+        host: "localhost",
+        username: "root",
+        password: "ITHB@23eUJ9QZapkFzd",
+        database: "clashtracker"
+    },
+    mongoDB: {
+        url: "mongodb+srv://shavy:sarvesh4452@cluster0.jh91n.mongodb.net/?retryWrites=true&w=majority",
+        clanDB: "linking",
+        playerDB: "player_linking",
+        collection: "whatshapp"
+    },
+    clashApi: {
+        username: "admin@clashpanda.xyz",
+        password: "ClashPanda@0710",
+        keyname: "dev backend"
+    },
+    jwt_secret: "kbvgedcyxuhjvgushdxhsfxdyguydsgxbuywgsyugbwquiu09w7878yhhyugduhudhytgysgysgftyfg",   
+    webhook: "https://discord.com/api/webhooks/1193276775506116609/yO4ed16TkJPVdTvpFxj7zJfvmGFpsOPPo4n9QoJ8g_qtXsf7vf4m_vavWcy9cjWYBSgd"
+};
+
+module.exports = config;
+EOL
+    echo "$CONFIG_FILE created successfully."
+}
+}
 
 # Step 5: Restart Application with PM2
 echo "Restarting application with PM2..."
