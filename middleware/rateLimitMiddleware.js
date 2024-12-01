0842746b9c00a905de99639562cbd5e5d02ad2c0
Shavy <shavygaming@gmail.com>
2024-12-01 07:53:52 +0530

    Increasing ratelimit

--- middleware/rateLimitMiddleware.js
+++ middleware/rateLimitMiddleware.js
@@ -2,11 +2,13 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 500, // limit each IP to 100 requests per windowMs
   message: {
     success: false,
     message: 'Too many requests, please try again later.'
-  }
+  },
+  standardHeaders: true, // Enable `RateLimit-*` headers
+  legacyHeaders: false, // Disable `X-RateLimit-*` headers
 });
 
 module.exports = limiter;
