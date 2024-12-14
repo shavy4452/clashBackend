2cbbbbb364bbb6f099e6455577e3393764ab6719
Shavy <shavygaming@gmail.com>
2024-12-14 13:20:37 +0530

    Updating Ratelimit

--- middleware/rateLimitMiddleware.js
+++ middleware/rateLimitMiddleware.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500, // limit each IP to 100 requests per windowMs
+  max: 5000000, // limit each IP to 100 requests per windowMs
   message: {
     success: false,
     message: 'Too many requests, please try again later.'
